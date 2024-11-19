a76ef73701acf525a17d7b447fd8d9ce9174f1d4
JosemarPascoal <josemarpas26@gmail.com>
2024-11-19 22:51:30 +0100

    enviando ex01

---
+++ ex03/its_even/Program.cs
@@ -0,0 +1,18 @@
+﻿class Program
+{
+static int Main(string[] argv)
+{
+    Console.WriteLine("Insira um numero: ");
+    int n = 0;
+    n = int.Parse(argv[0]);
+    if (n % 2 == 0)
+    {
+        Console.WriteLine($"{n} é um número primo.");
+    }
+    else
+    {
+        Console.WriteLine($"{n} não é um número primo.");
+    }
+    return (0);
+}
+}
